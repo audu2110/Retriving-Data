3d5e3ba9d6ab1a6ed67f1f976f36068b50745321
Audumbar <audumbarak2000@gmail.com>
2023-01-28 15:26:57 +0530

    First Commit

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'ecommerce_shop',
+    password: 'AUDUMBAR'
+});
+
+module.exports = pool.promise();
